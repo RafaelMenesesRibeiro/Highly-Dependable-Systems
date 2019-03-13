2068261363dbce743386a1f0da6575ec24cbc4bb
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-03-13 15:41:11 +0000

    Project Starting Point

---
+++ hds-project/hds-client/src/main/java/client/domain/Client.java
@@ -0,0 +1,9 @@
+package client.domain;
+
+public class Client {
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+}
---
+++ hds-project/hds-server/src/main/java/server/domain/Server.java
@@ -0,0 +1,11 @@
+package server.domain;
+
+public class Server {
+    private String ip;
+    private String port;
+
+    public Server(String ip, String port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
