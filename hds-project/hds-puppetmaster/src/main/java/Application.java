9e03c61a308ac62033c512a1e628ad80ac0801c3
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-14 12:17:54 +0000

    Basic DB interaction in hds-server

--- hds-project/hds-puppetmaster/src/main/java/Application.java
+++ hds-project/hds-puppetmaster/src/main/java/Application.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+
 import client.domain.Client;
 import server.domain.Server;
 
@@ -6,5 +8,9 @@ public class Application {
 		System.out.println("Puppet Master's Main");
     	Server server = new Server("localhost", "8014");
     	Client client = new Client("client1");
+
+    	Connection conn = server.connecToDB();
+    	String query = "SELECT * FROM users";
+    	server.QueryDB(conn, query);
     }
 }
--- hds-project/hds-server/src/main/java/server/domain/Server.java
+++ hds-project/hds-server/src/main/java/server/domain/Server.java
@@ -1,5 +1,7 @@
 package server.domain;
 
+import java.sql.*;
+
 public class Server {
     private String ip;
     private String port;
@@ -8,5 +10,38 @@ public class Server {
         this.ip = ip;
         this.port = port;
         System.out.println(String.format("Created server with ip=%s and port=%s", ip, port));
+    }
+
+    public Connection connecToDB() {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        // TODO - Moved user and password to .properties when using AWS DB.
+        String user = "postgres";
+        String password = "macaco90";
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void QueryDB(Connection conn, String query) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()) {
+                System.out.println(String.format("RESULT: %s", rs.getString("userid")));
+            }
+            conn.close();
+            stmt.close();
+            rs.close();
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
