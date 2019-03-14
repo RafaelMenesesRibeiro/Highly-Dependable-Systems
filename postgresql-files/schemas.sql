d2b93c82e8d238148f9f71856237c1b7e94f195f
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-14 12:30:33 +0000

    Improved schemas with new tables

--- postgresql-files/schemas.sql
+++ postgresql-files/schemas.sql
@@ -1,5 +1,7 @@
+drop table if exists ownership cascade;
 drop table if exists certificates cascade;
 drop table if exists users cascade;
+drop table if exists goods cascade;
 
 ----------------------------------------
 -- Table Creation
@@ -27,4 +29,18 @@ CREATE TABLE certificates (
 	certificateSignature varchar(1024) NOT NULL,
 	CONSTRAINT pk_certificates PRIMARY KEY(userId),
 	CONSTRAINT fk_users FOREIGN KEY(userId) REFERENCES users(userId)
+);
+
+CREATE TABLE goods (
+	goodId varchar(50),
+	onSale boolean,
+	CONSTRAINT pk_goods PRIMARY KEY (goodId)
+);
+
+CREATE TABLE ownership (
+	goodId varchar(50),
+    userId varchar(50),
+    CONSTRAINT pk_ownership PRIMARY KEY (goodId),
+    CONSTRAINT fk_ownership_goodId FOREIGN KEY (goodId) REFERENCES goods(goodId),
+    CONSTRAINT fk_ownership_userID FOREIGN KEY (userId) REFERENCES users(userId)
 );
