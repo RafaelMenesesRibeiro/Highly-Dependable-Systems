bc6de491735921de11881c40b5b1a6ad67752228
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-15 23:10:44 +0000

    Improved error handling

--- hds-project/hds-infrastructure/src/main/java/database/domain/Database.java
+++ hds-project/hds-infrastructure/src/main/java/database/domain/Database.java
@@ -1,7 +1,5 @@
 package database.domain;
 
-import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -9,6 +7,8 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+
+import database.exception.*;
 
 public class Database {
 	private Connection conn;
@@ -19,7 +19,8 @@ public class Database {
 
 	public Database() {
 		FetchProperties("aws-rds-db.properties");
-		conn = connecToDB();
+		this.conn = connecToDB();
+		System.out.println("Connected to the PostgreSQL server successfully.");
 
 		// TODO - Find out why postgres doesn't create tables with this. //
 		//String creationQuery = GetQuery("schemas.sql");
@@ -31,7 +32,12 @@ public class Database {
 		System.out.println(String.format("Database instance %s created. Endpoint is %s.", name, endpoint));
 	}
 
-	private void FetchProperties(String propertiesFilename) {
+	String getEndpoint() {return this.endpoint; }
+	String getName() {return this.name; }
+	String getUsername() {return this.username; }
+	String getPassword() {return this.password; }
+
+	private void FetchProperties(String propertiesFilename) throws InvalidPropertyException {
 		Properties properties = new Properties();
 		InputStream input = null;
 		try {
@@ -44,11 +50,15 @@ public class Database {
 			}
 			properties.load(input);
 			endpoint = properties.getProperty("dbEndpoint");
+			if (endpoint == null) {throw new InvalidPropertyException("'dbEndpoint' property was read as 'null' in .properties"); }
 			name = properties.getProperty("dbName");
+			if (name == null) {throw new InvalidPropertyException("'dbName' property was read as 'null' in .properties"); }
 			username = properties.getProperty("dbUsername");
+			if (username == null) {throw new InvalidPropertyException("'dbUsername' property was read as 'null' in .properties"); }
 			password = properties.getProperty("dbPassword");
+			if (password == null) {throw new InvalidPropertyException("'dbPassword' property was read as 'null' in .properties"); }
 		}
-		catch (IOException ex) {
+		catch (IOException | InvalidPropertyException ex) {
 			System.out.println(ex.getMessage());
 			System.exit(1);
 		}
@@ -63,21 +73,23 @@ public class Database {
 		}
 	}
 
-	public Connection connecToDB() {
+	private Connection connecToDB() {
 		String url = String.format("jdbc:postgresql:%s/%s", this.endpoint, this.name);
-
-		Connection conn = null;
 		try {
-			conn = DriverManager.getConnection(url, this.username, this.password);
-			System.out.println("Connected to the PostgreSQL server successfully.");
+			return DriverManager.getConnection(url, this.username, this.password);
 		}
-		catch (SQLException e) { System.out.println(e.getMessage()); }
-		return conn;
+		catch (SQLException sqlex) {
+			System.out.println(sqlex.getMessage());
+			System.exit(1);
+		}
+		return null;
 	}
 
-	public List<String> QueryDB(Connection conn, String query, String returnColumn) {
+	private List<String> QueryDB(Connection conn, String query, String returnColumn)
+			throws DBClosedConnectionException, DBConnectionRefusedException, DBSQLException {
 		List<String> results = new ArrayList<String>();
 		try {
+			// TODO - Create pre prepared statements. //
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			while(rs.next()) {
@@ -87,38 +99,88 @@ public class Database {
 			stmt.close();
 			rs.close();
 		}
-		catch (SQLException ex) { System.out.println(ex.getMessage()); }
+		// TODO - Is exiting the best approach for this exception? //
+		catch (SQLTimeoutException sqltex) {
+			System.out.println(sqltex.getMessage());
+			System.exit(1);
+		}
+		catch (SQLException sqlex) {
+			switch (String.valueOf((sqlex.getErrorCode()))) {
+				case "08000": // connection_exception
+				case "08003": // connection_does_not_exist
+				case "08006": // connection_failure
+				case "08001": // qlclient_unable_to_establish_sqlconnection
+					throw new DBClosedConnectionException(sqlex.getMessage());
+				case "08004": // sqlserver_rejected_establishment_of_sqlconnection
+				case "08007": // transaction_resolution_unknown
+				case "08P01": // protocol_violation
+					throw new DBConnectionRefusedException(sqlex.getMessage());
+				case "2F000": // sql_routine_exception
+				case "2F003": // prohibited_sql_statement_attempted
+					throw new DBSQLException(sqlex.getMessage());
+				default:
+					System.out.println(sqlex.getMessage());
+					System.exit(1);
+			}
+		}
 		return results;
 	}
 
 	private String GetQuery(String filename) {
 		try {
+			if (filename == null) {
+				throw new NullPointerException("The name of the file containing the query was null.");
+			}
 			InputStream input = getClass().getClassLoader().getResourceAsStream(filename);
+			if (input == null) {
+				throw new NullPointerException("No file with the given name was found (" + filename + ").");
+			}
 			BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName(StandardCharsets.UTF_8.name())));
-			String data = "";
+			StringBuilder data = new StringBuilder();
 			String line;
 			while ((line = reader.readLine()) != null) {
-				data += line;
+				data.append(line);
 			}
-			return data;
+			return data.toString();
 		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return "";
+		catch (NullPointerException | IOException ex) {
+			System.out.println(ex.getMessage());
+			System.exit(1);
 		}
+		return "";
 	}
 
-	public String getCurrentOwner(String goodID) {
+	public String getCurrentOwner(String goodID)
+			throws DBClosedConnectionException, DBConnectionRefusedException, DBSQLException, InvalidQueryParameterException {
+		if (goodID == null || goodID.equals("")) {
+			throw new InvalidQueryParameterException("The parameter 'goodID' in query 'getCurrentOwner' is either null or an empty string.");
+		}
 		String query = "select (ownership.userId) from ownership where ownership.goodId = '" + goodID + "'";
-		System.out.println(query);
-		List<String> results = QueryDB(this.conn, query, "userId");
-		return results.get(0);
+		try {
+			List<String> results = QueryDB(this.conn, query, "userId");
+			return results.get(0);
+		}
+		// DBClosedConnectionException | DBConnectionRefusedException | DBSQLException are ignored to be
+		// caught up the chain.
+		catch (IndexOutOfBoundsException ioobex) {
+			throw new DBNoResultsException("The query \"" + query + "\" returned no results.");
+		}
 	}
 
-	public Boolean getIsOnSale(String goodID) {
+	public Boolean getIsOnSale(String goodID)
+			throws DBClosedConnectionException, DBConnectionRefusedException, DBSQLException, InvalidQueryParameterException {
+		if (goodID == null || goodID.equals("")) {
+			throw new InvalidQueryParameterException("The parameter 'goodID' in query 'getIsOnSale' is either null or an empty string.");
+		}
 		String query = "select (goods.onSale) from goods where goods.goodId = " + goodID;
-		System.out.println(query);
-		List<String> results = QueryDB(this.conn, query, "onSale");
-		return results.get(0) == "true";
+		try {
+			List<String> results = QueryDB(this.conn, query, "onSale");
+			return results.get(0).equals("true");
+		}
+		// DBClosedConnectionException | DBConnectionRefusedException | DBSQLException are ignored to be
+		// caught up the chain.
+		catch (IndexOutOfBoundsException ioobex) {
+			throw new DBNoResultsException("The query \"" + query + "\" returned no results.");
+		}
 	}
 }
