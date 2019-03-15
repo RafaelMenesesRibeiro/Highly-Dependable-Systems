c7626c6464b24b05c6e38ef65fe9c285b7cbd2b3
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-15 10:47:07 +0000

    Server now has database reference
    - Implemented some basic methods to test the validity of transactions
    - To complete

--- hds-project/hds-server/src/main/java/server/domain/Server.java
+++ hds-project/hds-server/src/main/java/server/domain/Server.java
@@ -1,12 +1,33 @@
 package server.domain;
 
-public class Server {
-    private String ip;
-    private String port;
+import database.domain.Database;
+import server.services.local.dataobjects.TransactionData;
 
-    public Server(String ip, String port) {
-        this.ip = ip;
-        this.port = port;
-        System.out.println(String.format("Created server with ip=%s and port=%s", ip, port));
-    }
+public class Server {
+	private Database database;
+	private String ip;
+	private String port;
+
+	public Server(String ip, String port, Database database) {
+		this.ip = ip;
+		this.port = port;
+		this.database = database;
+		System.out.println(String.format("Created server with ip=%s and port=%s", ip, port));
+	}
+
+	// TODO - Change to private after initial testing. //
+	public boolean IsTransactionValid(TransactionData transaction) {
+		String sellerID = transaction.getSellerID();
+		String goodID = transaction.getGoodID();
+		return IsSellerOwner(sellerID, goodID) && IsGoodOnSale(goodID);
+	}
+
+	private boolean IsSellerOwner(String sellerID, String goodID) {
+		String currentOwner = database.getCurrentOwner(goodID);
+		return currentOwner == sellerID;
+	}
+
+	private boolean IsGoodOnSale(String goodID) {
+		return database.getIsOnSale(goodID);
+	}
 }
