1d9a2a14e6cbf54861dd24717d94458962a82f96
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-15 10:47:21 +0000

    Fixed constructor

--- hds-project/hds-puppetmaster/src/main/java/Application.java
+++ hds-project/hds-puppetmaster/src/main/java/Application.java
@@ -2,13 +2,21 @@ import java.sql.Connection;
 
 import client.domain.Client;
 import database.domain.Database;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import server.domain.Server;
+import server.services.local.dataobjects.TransactionData;
 
 public class Application {
     public static void main(String[] args) {
 		System.out.println("Puppet Master's Main");
-    	Server server = new Server("localhost", "8014");
+		Database db = new Database();
+		Server server = new Server("localhost", "8014", db);
     	Client client = new Client("client1");
-    	Database db = new Database();
+
+
+		TransactionData transaction = new TransactionData("user1", "user2", "good1");
+		Boolean res = server.IsTransactionValid(transaction);
+		System.out.println("The transaction validity was: " + res.booleanValue());
+
     }
 }
