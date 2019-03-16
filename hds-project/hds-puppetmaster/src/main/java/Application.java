5ead3977cfa9a642854500fbcbcfa5390931ec00
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-16 15:07:46 +0000

    Changed pseudo test to check result

--- hds-project/hds-puppetmaster/src/main/java/Application.java
+++ hds-project/hds-puppetmaster/src/main/java/Application.java
@@ -9,12 +9,14 @@ public class Application {
     public static void main(String[] args) {
 		System.out.println("Puppet Master's Main");
 		Database db = new Database();
+		// db.CreateTables();
+		// db.PopulateTables();
+
 		Server server = new Server("localhost", "8014", db);
     	Client client = new Client("client1");
 
-		TransactionData transaction = new TransactionData("user1", "user2", "good1");
+		TransactionData transaction = new TransactionData("user3", "user2", "good3");
 		boolean res = server.IsTransactionValid(transaction);
 		System.out.println("The transaction validity was: " + res);
-
     }
 }
