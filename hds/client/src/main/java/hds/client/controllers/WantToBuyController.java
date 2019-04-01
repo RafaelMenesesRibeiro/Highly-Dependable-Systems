4e4cfcbc505db22a6daa02244057a03bb78b7317
DiogoFSVilela <diogofsvilela@gmail.com>
2019-04-01 22:35:10 +0100

    Development on WantToBuyController

--- hds/client/src/main/java/hds/client/controllers/WantToBuyController.java
+++ hds/client/src/main/java/hds/client/controllers/WantToBuyController.java
@@ -2,6 +2,7 @@ package hds.client.controllers;
 
 import hds.client.helpers.ClientProperties;
 import hds.security.SecurityManager;
+import hds.security.domain.BasicResponse;
 import hds.security.domain.SignedTransactionData;
 import hds.security.domain.TransactionData;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,21 +16,16 @@ public class WantToBuyController {
     private static final String OPERATION = "wantToBuy";
 
     @PostMapping(value = "/wantToBuy") //TODO: IT'S NOT VOID!!!!!!!!!!!
-    public void wantToBuy(@RequestBody SignedTransactionData signedTransactionData) {
-        byte[] buyerSignature = signedTransactionData.getBuyerSignature();
+    public BasicResponse wantToBuy(@RequestBody SignedTransactionData signedTransactionData) {
         TransactionData transactionData = signedTransactionData.getPayload();
         String sellerID = transactionData.getSellerID();
         String buyerID = transactionData.getBuyerID();
         String goodID = transactionData.getGoodID();
+        BasicResponse payload;
 
         try {
-            byte[] transactionDataBytes = SecurityManager.getByteArray(transactionData);
-            PublicKey buyerPublicKey = SecurityManager.getPublicKeyFromResource(buyerID);
-            PrivateKey sellerPrivateKey = SecurityManager.getPrivateKeyFromResource(ClientProperties.getPort());
-            byte[] sellerSignature = SecurityManager.signData(sellerPrivateKey, transactionDataBytes);
+            payload = execute(signedTransactionData, sellerID, buyerID, goodID);
 
-            SecurityManager.verifySignature(buyerPublicKey, buyerSignature, transactionDataBytes);
-            signedTransactionData.setSellerSignature(sellerSignature);
             //TODO: SEND TO NOTARY
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
@@ -42,6 +38,23 @@ public class WantToBuyController {
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
+
+        return payload;
+    }
+
+    private BasicResponse execute(SignedTransactionData signedTransactionData, String sellerID, String buyerID, String goodID)
+            throws IOException, InvalidKeySpecException, NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+        byte[] buyerSignature = signedTransactionData.getBuyerSignature();
+        byte[] transactionDataBytes = SecurityManager.getByteArray(signedTransactionData.getPayload());
+        PublicKey buyerPublicKey = SecurityManager.getPublicKeyFromResource(buyerID);
+        PrivateKey sellerPrivateKey = SecurityManager.getPrivateKeyFromResource(ClientProperties.getPort());
+        byte[] sellerSignature = SecurityManager.signData(sellerPrivateKey, transactionDataBytes);
+
+        if (!SecurityManager.verifySignature(buyerPublicKey, buyerSignature, transactionDataBytes)) {
+
+        }
+
+        signedTransactionData.setSellerSignature(sellerSignature);
     }
 
 }
