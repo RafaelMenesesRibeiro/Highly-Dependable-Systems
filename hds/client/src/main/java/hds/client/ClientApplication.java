109284a743bdfd0c2ce10853d6e02c5113078ed5
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-01 14:34:36 +0100

    Update ClientApplication.java

--- hds/client/src/main/java/hds/client/ClientApplication.java
+++ hds/client/src/main/java/hds/client/ClientApplication.java
@@ -1,14 +1,19 @@
 package hds.client;
 
 import hds.client.helpers.ClientProperties;
+import jdk.internal.util.xml.impl.Input;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-import java.util.Properties;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 @SpringBootApplication
 public class ClientApplication {
+    private static int input;
+    private static boolean acceptingCommands = true;
+    private static Scanner inputScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         String portId = args[0];
         String maxPortId = args[1];
@@ -19,5 +24,35 @@ public class ClientApplication {
         SpringApplication app = new SpringApplication(ClientApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", portId));
         app.run(args);
+
+        while(acceptingCommands) {
+
+            System.out.print("[o] Press '1' to get state of good, '2' to buy a good, '3' to quit... \n");
+
+            try {
+                input = inputScanner.nextInt();
+            } catch (NoSuchElementException | IllegalStateException exc) {
+                continue;
+            }
+
+            switch (input) {
+                case 1:
+                    getStateOfGood();
+                case 2:
+                    buyGood();
+                case 3:
+                    acceptingCommands = false;
+                default:
+                    break;
+            }
+        }
+
+        acceptingCommands = true;
     }
- }
+
+    private static void getStateOfGood() {
+    }
+
+    private static void buyGood() {
+    }
+}
