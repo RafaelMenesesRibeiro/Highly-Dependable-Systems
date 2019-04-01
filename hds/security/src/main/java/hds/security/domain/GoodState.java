b6eeed45f60699c03287be0e2602397bd8aa2536
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-01 18:12:41 +0100

    Renamed StateOfGood to GoodState

--- hds/security/src/main/java/hds/security/domain/GoodState.java
+++ hds/security/src/main/java/hds/security/domain/GoodState.java
@@ -1,10 +1,10 @@
 package hds.security.domain;
 
-public class StateOfGood extends BasicResponse{
+public class GoodState extends BasicResponse{
 	private final String ownerId;
 	private final boolean onSale;
 
-	public StateOfGood(int code, String message, String operation, String ownerId, boolean onSale) {
+	public GoodState(int code, String message, String operation, String ownerId, boolean onSale) {
 		super(code, message, operation);
 		this.ownerId = ownerId;
 		this.onSale = onSale;
@@ -16,5 +16,13 @@ public class StateOfGood extends BasicResponse{
 
 	public boolean getOnSale() {
 		return onSale;
+	}
+
+	@Override
+	public String toString() {
+		return "GoodState{" +
+				"ownerId='" + ownerId + '\'' +
+				", onSale=" + onSale +
+				'}';
 	}
 }
