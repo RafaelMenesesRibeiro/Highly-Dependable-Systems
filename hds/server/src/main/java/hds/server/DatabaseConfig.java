3c92e41c5b1fc22f72bb5e5ce21010bd7fe6b2ed
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-04-02 22:35:39 +0100

    Work around for DB connection

--- hds/server/src/main/java/hds/server/DatabaseConfig.java
+++ hds/server/src/main/java/hds/server/DatabaseConfig.java
@@ -2,7 +2,6 @@ package hds.server;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -10,33 +9,19 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 @Profile("web")
 @Configuration
 @Component
 public class DatabaseConfig {
-	@Value("${spring.datasource.driverClassName}")
-	public String driver;
-	@Value("${spring.datasource.platform}")
-	public String platform;
-	@Value("${spring.datasource.url}")
-	public String url;
-	@Value("${spring.datasource.username}")
-	public String user;
-	@Value("${spring.datasource.password}")
-	public String password;
-
 	@Bean
-	public DataSource dataSource() {
+	public static DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(driver);
-		dataSource.setUrl(url);
-		dataSource.setUsername(user);
-		dataSource.setPassword(password);
+		dataSource.setDriverClassName(ServerApplication.getDriver());
+		dataSource.setUrl(ServerApplication.getUrl());
+		dataSource.setUsername(ServerApplication.getUser());
+		dataSource.setPassword(ServerApplication.getPassword());
 		return dataSource;
-	}
-
-	public Connection getConnection() throws Exception {
-		return dataSource().getConnection();
 	}
 }
--- hds/server/src/main/java/hds/server/helpers/DatabaseManager.java
+++ hds/server/src/main/java/hds/server/helpers/DatabaseManager.java
@@ -1,28 +1,23 @@
 package hds.server.helpers;
 
 import hds.server.DatabaseConfig;
-import hds.server.exception.DBConnectionRefusedException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.util.logging.Logger;
 
 public class DatabaseManager {
-	@Autowired
-	private ApplicationContext appContext;
+	private static DataSource dataSource;
 
 	private DatabaseManager() {
 		// This is here so the class can't be instantiated. //
 	}
 
 	public static Connection getConnection() throws SQLException {
-		return new DatabaseConfig().dataSource().getConnection();
+		// TODO - Improve error handling. //
+		if (dataSource == null) {
+			dataSource = DatabaseConfig.dataSource();
+		}
+		return dataSource.getConnection();
 	}
 }
