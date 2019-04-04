0e6be682964180bdf2637ad5116f82cfbe56ab5a
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-04 16:01:54 +0100

    Removed TODOs from someone else.

--- hds/client/src/main/java/hds/client/helpers/ConnectionManager.java
+++ hds/client/src/main/java/hds/client/helpers/ConnectionManager.java
@@ -50,7 +50,6 @@ public class ConnectionManager {
     public static SecureResponse getSecureResponse(HttpURLConnection connection) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonResponse = getJSONStringFromHttpResponse(connection);
-        //TODO: HERE!!!!
         return objectMapper.readValue(jsonResponse, SecureResponse.class);
     }
 
