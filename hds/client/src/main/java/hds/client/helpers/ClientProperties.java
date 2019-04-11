2cd83193ddb30e5b3de061b490f11ab48cfd085f
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-11 12:53:04 +0100

    Update ClientProperties.java

--- hds/client/src/main/java/hds/client/helpers/ClientProperties.java
+++ hds/client/src/main/java/hds/client/helpers/ClientProperties.java
@@ -1,9 +1,17 @@
 package hds.client.helpers;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+
+import static hds.security.ResourceManager.getPrivateKeyFromResource;
+
 public class ClientProperties {
 
     private static String portId;
     private static String maxPortId;
+    private static PrivateKey privateKey;
     public static final String HDS_NOTARY_HOST = "http://localhost:8000/";
     public static final String HDS_NOTARY_PORT = "8000";
     private ClientProperties() {}
@@ -15,6 +23,12 @@ public class ClientProperties {
     public static void setPort(String portId) {
         if (ClientProperties.portId != null) { throw new RuntimeException("port is 'final'"); }
         ClientProperties.portId = portId;
+        try {
+            ClientProperties.privateKey = getPrivateKeyFromResource(portId);
+        } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException e) {
+            System.out.println("Error loading privateKey from resources");
+            System.exit(-1);
+        }
     }
 
     public static String getMaxPortId() {
@@ -24,5 +38,9 @@ public class ClientProperties {
     public static void setMaxPortId(String maxPortId) {
         if (ClientProperties.maxPortId != null) { throw new RuntimeException("maxPortId is 'final'"); }
         ClientProperties.maxPortId = maxPortId;
+    }
+
+    public static PrivateKey getPrivateKey() {
+        return privateKey;
     }
 }
