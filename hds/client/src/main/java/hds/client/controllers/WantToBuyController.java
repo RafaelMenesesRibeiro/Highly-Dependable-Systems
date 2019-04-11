a019ada62303aa880b3be87783ff9a840ba92dae
DiogoFSVilela <diogofsvilela@gmail.com>
2019-04-11 19:27:56 +0100

    Remake of WantToBuyController

--- hds/client/src/main/java/hds/client/controllers/WantToBuyController.java
+++ hds/client/src/main/java/hds/client/controllers/WantToBuyController.java
@@ -1,16 +1,25 @@
 package hds.client.controllers;
 
+import ch.qos.logback.core.net.server.Client;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hds.client.exceptions.ResponseMessageException;
 import hds.client.helpers.ClientProperties;
-import hds.security.msgtypes.BasicMessage;
-import hds.security.msgtypes.ErrorResponse;
-import hds.security.msgtypes.SaleCertificateResponse;
-import hds.security.msgtypes.SaleRequestMessage;
+import hds.security.msgtypes.*;
+import org.json.JSONException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.security.SignatureException;
+
+import static hds.client.helpers.ClientProperties.HDS_NOTARY_HOST;
+import static hds.client.helpers.ClientProperties.getPrivateKey;
+import static hds.client.helpers.ConnectionManager.*;
 import static hds.security.DateUtils.generateTimestamp;
 import static hds.security.SecurityManager.isValidMessage;
+import static hds.security.SecurityManager.setMessageSignature;
 
 @RestController
 public class WantToBuyController {
@@ -27,8 +36,7 @@ public class WantToBuyController {
     }
 
     private BasicMessage trySell(SaleRequestMessage requestMessage) {
-        // TODO See your old execute method
-        return new SaleCertificateResponse(); // Whatever is returned by the server may even be an ErrorResponse
+        return execute(requestMessage);
     }
 
     private BasicMessage newErrorResponse(BasicMessage receivedRequest, String reason) {
@@ -42,6 +50,50 @@ public class WantToBuyController {
                 "bad request",
                 reason
         );
+    }
+
+    private BasicMessage execute(SaleRequestMessage requestMessage) {
+        ApproveSaleRequestMessage message = new ApproveSaleRequestMessage(
+                requestMessage.getTimestamp(),
+                requestMessage.getRequestID(),
+                requestMessage.getOperation(),
+                requestMessage.getFrom(),
+                requestMessage.getTo(),
+                requestMessage.getSignature(),
+                requestMessage.getGoodID(),
+                requestMessage.getBuyerID(),
+                requestMessage.getSellerID(),
+                generateTimestamp(),
+                "transferGood",
+                ClientProperties.getPort(),
+                ClientProperties.HDS_NOTARY_PORT,
+                ""
+        );
+
+        try {
+            setMessageSignature(getPrivateKey(), message);
+        } catch (IOException | SignatureException e) {
+            return newErrorResponse(requestMessage, "The seller has thrown an exception while signing the message");
+        }
+
+        try {
+            HttpURLConnection connection = initiatePOSTConnection(HDS_NOTARY_HOST + "transferGood");
+            sendPostRequest(connection, newJSONObject(message));
+            BasicMessage responseMessage = getResponseMessage(connection, Expect.SALE_CERT_RESPONSE);
+            String validationResult = isValidMessage(requestMessage.getFrom(), responseMessage);
+            if (!validationResult.equals("")) {
+                return newErrorResponse(requestMessage, "The seller has encountered the following error validating response from server :" + validationResult);
+            }
+
+            System.out.println("[o] " + responseMessage.toString());
+            return responseMessage;
+        } catch (JsonProcessingException | JSONException e) {
+            return newErrorResponse(requestMessage, "The seller has thrown an exception while creating the json to send to the notary");
+        } catch (IOException e) {
+            return newErrorResponse(requestMessage, "The seller has thrown an exception while reading/writing message from/to notary");
+        } catch (ResponseMessageException e) {
+            return newErrorResponse(requestMessage, "The seller has thrown an exception while receiving the response from notary");
+        }
     }
 
     /*
