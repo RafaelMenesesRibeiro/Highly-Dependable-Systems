0cee8e8dcbcfe1640b343c23944632a417bdd049
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-11 15:11:05 +0100

    Update SecurityManager.java

--- hds/security/src/main/java/hds/security/SecurityManager.java
+++ hds/security/src/main/java/hds/security/SecurityManager.java
@@ -4,6 +4,7 @@ import hds.security.msgtypes.BasicMessage;
 
 import java.io.IOException;
 import java.security.PrivateKey;
+import java.security.SignatureException;
 
 import static hds.security.ConvertUtils.bytesToBase64String;
 import static hds.security.ConvertUtils.objectToByteArray;
@@ -11,7 +12,7 @@ import static hds.security.CryptoUtils.signData;
 
 public class SecurityManager {
 
-    public static BasicMessage setMessageSignature(PrivateKey privateKey, BasicMessage message) throws IOException {
+    public static BasicMessage setMessageSignature(PrivateKey privateKey, BasicMessage message) throws IOException, SignatureException {
         message.setSignature(bytesToBase64String(signData(privateKey, objectToByteArray(message))));
         return message;
     }
