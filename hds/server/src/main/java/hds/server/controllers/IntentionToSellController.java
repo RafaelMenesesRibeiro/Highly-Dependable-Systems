71940cd1c7c4b1403e8502573329338d933ae245
DiogoFSVilela <diogofsvilela@gmail.com>
2019-04-12 00:18:08 +0100

    Fix bug where verification of signature was done with the signature yet in the payload

--- hds/server/src/main/java/hds/server/controllers/IntentionToSellController.java
+++ hds/server/src/main/java/hds/server/controllers/IntentionToSellController.java
@@ -84,7 +84,11 @@ public class IntentionToSellController {
 				ErrorResponse payload = new ErrorResponse(generateTimestamp(), ownerData.getRequestID(), OPERATION, FROM_SERVER, ownerData.getFrom(), "", ControllerErrorConsts.NO_PERMISSION, reason);
 				return new MetaResponse(403, payload);
 			}
-			boolean res = isClientWilling(sellerID, ownerData.getSignature(), ownerData);
+
+			String signature = ownerData.getSignature();
+			ownerData.setSignature("");
+			boolean res = isClientWilling(sellerID, signature, ownerData);
+			ownerData.setSignature(signature);
 			if (!res) {
 				conn.rollback();
 				String reason = "The Seller's signature is not valid.";
