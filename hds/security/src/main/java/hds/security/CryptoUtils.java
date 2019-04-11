5b6506d4213e0bdbfd6633c055ee5124cfcbed1a
DiogoFSVilela <diogofsvilela@gmail.com>
2019-04-11 12:37:43 +0100

    Add differentiation between signing with client and server

--- hds/security/src/main/java/hds/security/CryptoUtils.java
+++ hds/security/src/main/java/hds/security/CryptoUtils.java
@@ -1,17 +1,26 @@
 package hds.security;
 
+import hds.security.msgtypes.BasicMessage;
+
+import java.io.IOException;
 import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 
 public class CryptoUtils {
     private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
 
-    static boolean authenticateSignature(PublicKey key, byte[] signedData, byte[] testData)
-            throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+    public static boolean isAuthenticResponseFromClient(BasicMessage secureResponse, String nodeId)
+            throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
 
-        Signature sign = Signature.getInstance(SIGNATURE_ALGORITHM);
-        sign.initVerify(key);
-        sign.update(testData);
-        return sign.verify(signedData);
+        PublicKey HDSPublicKey = ResourceManager.getPublicKeyFromResource(nodeId);
+        return authenticateSignature(HDSPublicKey, secureResponse.getSignature(), secureResponse.getPayload());
+    }
+
+    public static boolean isAuthenticResponseFromServer(BasicMessage secureResponse) throws CertificateException, IOException {
+        X509Certificate serverCert = ResourceManager.getCertificateFromResource();
+        return authenticateSignature(serverCert, secureResponse.getSignature(), secureResponse.getPayload());
     }
 
     public static byte[] signData(PrivateKey key, byte[] data)
@@ -23,9 +32,28 @@ public class CryptoUtils {
         return sign.sign();
     }
 
+    private static boolean authenticateSignature(PublicKey key, byte[] signedData, byte[] testData)
+            throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+
+        Signature sign = Signature.getInstance(SIGNATURE_ALGORITHM);
+        sign.initVerify(key);
+        sign.update(testData);
+        return sign.verify(signedData);
+    }
+
     public static boolean authenticateSignature(PublicKey key, String signedData, Object payload) {
         try {
             return authenticateSignature(key, ConvertUtils.base64StringToBytes(signedData), ConvertUtils.objectToByteArray(payload));
+        } catch (Exception exc) {
+            System.out.println("[xxx] Unexpected error getting payload bytes for signature verification.");
+            return false;
+        }
+    }
+
+    public static boolean authenticateSignature(X509Certificate certificate, String signedData, Object payload) {
+        try {
+            certificate.checkValidity();
+            return authenticateSignature(certificate.getPublicKey(), ConvertUtils.base64StringToBytes(signedData), ConvertUtils.objectToByteArray(payload));
         } catch (Exception exc) {
             System.out.println("[xxx] Unexpected error getting payload bytes for signature verification.");
             return false;
