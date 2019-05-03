a8d63a1da5ab5e7ea636727cccb07ef04a8fff12
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-03 21:31:35 +0100

    Updated PseudoPuppetMaster to create clients as well

---
+++ hds/PseudoPuppetMaster.bat
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ECHO OFF
+SetLocal EnableDelayedExpansion
+
+set initialClientPort=8001
+set initialServerPort=9000
+
+set /p numberOfClients="Enter number of clients to create : "
+set /p maxNumberOfClients="Enter maximum number of clients in the system: "
+set /p numberOfServers="Enter number of servers to create : "
+
+set /A sum=%initialClientPort%+%maxNumberOfClients%-1
+set /A maxClientPort=%sum%
+echo First Client Port = %initialClientPort%
+echo Final Client Port = %maxClientPort%
+
+set /A sum=%initialServerPort%+%numberOfServers%-1
+set /A maxServerPort=%sum%
+echo First Server Port = %initialServerPort%
+echo Final Server Port = %maxServerPort%
+
+
+call mvn clean compile install -DskipTests
+
+FOR /L %%N IN (1, 1, %numberOfServers%) DO (
+   set number=%%N
+   set /A s=!number!+%initialServerPort%-1
+   echo s=!s!
+   start cmd /c "cd server && mvn spring-boot:run -Dspring-boot.run.arguments=!s!,%maxClientPort% && pause"
+)
+
+FOR /L %%N IN (1, 1, %numberOfClients%) DO (
+   set number=%%N
+   set /A s=!number!+%initialClientPort%-1
+   echo s=!s!
+   start cmd /c "cd client && mvn spring-boot:run -Dspring-boot.run.arguments=!s!,%maxClientPort%,%maxServerPort% && pause"
+)
+
+pause
--- hds/client/src/main/java/hds/client/helpers/ClientProperties.java
+++ hds/client/src/main/java/hds/client/helpers/ClientProperties.java
@@ -1,9 +1,11 @@
 package hds.client.helpers;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
 
 import static hds.security.ResourceManager.getPrivateKeyFromResource;
 
@@ -11,10 +13,14 @@ public class ClientProperties {
 
     private static String portId;
     private static String maxPortId;
+    private static ArrayList<Integer> notaryReplicasPorts;
     private static PrivateKey privateKey;
     public static final String HDS_BASE_HOST = "http://localhost:";
     public static final String HDS_NOTARY_HOST = "http://localhost:8000/";
+    // TODO - Remove this. //
     public static final String HDS_NOTARY_PORT = "8000";
+    public static final int HDS_NOTARY_REPLICAS_FIRST_PORT = 9000;
+
     private ClientProperties() {}
 
     public static String getPort() {
@@ -44,4 +50,26 @@ public class ClientProperties {
     public static PrivateKey getPrivateKey() {
         return privateKey;
     }
+
+
+    public ArrayList<Integer> getNotaryReplicas() {
+        return ClientProperties.notaryReplicasPorts;
+    }
+
+    public void updateNotaryReplicas(ArrayList<Integer> newSet) {
+        notaryReplicasPorts = newSet;
+    }
+
+    public void removeNotaryReplica(int replicaPort) {
+        ClientProperties.notaryReplicasPorts.remove(replicaPort);
+    }
+
+    public static void initializeNotaryReplicasPortsList(int maxServerPort) {
+        ArrayList<Integer> replicas = new ArrayList<>();
+        for (int replicaPort = HDS_NOTARY_REPLICAS_FIRST_PORT; replicaPort <= maxServerPort; replicaPort++) {
+            replicas.add(replicaPort);
+        }
+        ClientProperties.notaryReplicasPorts = replicas;
+    }
+
 }
