61774cbbd22391873a80ad736eafc660c90ac1fb
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-03 21:27:06 +0100

    Create GetStateOfGoodTask.java

---
+++ hds/client/src/main/java/hds/client/domain/GetStateOfGoodTask.java
@@ -0,0 +1,22 @@
+package hds.client.domain;
+
+import hds.security.msgtypes.BasicMessage;
+
+import java.net.HttpURLConnection;
+import java.util.concurrent.Callable;
+
+import static hds.client.helpers.ConnectionManager.*;
+
+public class GetStateOfGoodTask implements Callable<BasicMessage> {
+    private String address;
+
+    public GetStateOfGoodTask(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public BasicMessage call() throws Exception {
+        HttpURLConnection connection = initiateGETConnection(address);
+        return getResponseMessage(connection, Expect.GOOD_STATE_RESPONSE);
+    }
+}
