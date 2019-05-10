f9d4ceb3d2f0c9b932a89c5ddb8ab4be43109ba9
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-10 14:15:44 +0100

    Update IntentionToSellCallable.java

--- hds/client/src/main/java/hds/client/domain/IntentionToSellCallable.java
+++ hds/client/src/main/java/hds/client/domain/IntentionToSellCallable.java
@@ -4,22 +4,41 @@ import hds.security.msgtypes.BasicMessage;
 import hds.security.msgtypes.OwnerDataMessage;
 
 import java.net.HttpURLConnection;
+import java.security.SignatureException;
 import java.util.concurrent.Callable;
 
 import static hds.client.helpers.ClientProperties.getPort;
 import static hds.client.helpers.ClientProperties.getPrivateKey;
 import static hds.client.helpers.ConnectionManager.*;
+import static hds.security.ConvertUtils.bytesToBase64String;
+import static hds.security.CryptoUtils.signData;
+import static hds.security.SecurityManager.newWriteOperation;
 import static hds.security.SecurityManager.setMessageSignature;
 
 public class IntentionToSellCallable implements Callable<BasicMessage> {
     private static final String OPERATION = "intentionToSell";
     private static final String REQUEST_ENDPOINT = "http://localhost:%s/%s";
-    private final String replicaId;
-    private final OwnerDataMessage message;
+    private static final Boolean onSale;
 
-    public IntentionToSellCallable(long timestamp, String requestId, String replicaId, String goodId) {
+    private final OwnerDataMessage message;
+    private final String replicaId;
+
+    static {
+        onSale = Boolean.TRUE;
+    }
+
+    public IntentionToSellCallable(long timestamp, String requestId, String replicaId, String goodId, int logicalTimestamp)
+            throws SignatureException {
+
         this.replicaId = replicaId;
-        this.message = newOwnerDataMessage(timestamp, requestId, replicaId, goodId);
+
+        byte[] writeOpSignature = signData(
+                getPrivateKey(), newWriteOperation(onSale, getPort(), logicalTimestamp).toString().getBytes()
+        );
+
+        this.message = newOwnerDataMessage(
+                timestamp, requestId, replicaId, goodId, logicalTimestamp, bytesToBase64String(writeOpSignature)
+        );
     }
 
     @Override
@@ -30,16 +49,25 @@ public class IntentionToSellCallable implements Callable<BasicMessage> {
         return (BasicMessage) getResponseMessage(connection, Expect.BASIC_MESSAGE);
     }
 
-    private OwnerDataMessage newOwnerDataMessage(long timestamp, String requestId, String to, String goodId) {
+    private OwnerDataMessage newOwnerDataMessage(long timestamp,
+                                                 String requestId,
+                                                 String to,
+                                                 String goodId,
+                                                 int logicalTimestamp,
+                                                 String writeOperationJson) {
+
         return new OwnerDataMessage(
                 timestamp,
                 requestId,
                 OPERATION,
-                getPort(),
+                getPort(), // from
                 to,
                 "",
                 goodId,
-                getPort()
+                getPort(), // owner
+                logicalTimestamp,
+                onSale,
+                writeOperationJson
         );
     }
 }
