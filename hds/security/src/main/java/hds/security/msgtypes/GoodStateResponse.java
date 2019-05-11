8c17c93856809cb4cb1f57ec9ed267c52d7c26f4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-11 14:36:42 +0100

    Update GoodStateResponse.java

--- hds/security/src/main/java/hds/security/msgtypes/GoodStateResponse.java
+++ hds/security/src/main/java/hds/security/msgtypes/GoodStateResponse.java
@@ -15,8 +15,26 @@ public class GoodStateResponse extends BasicMessage implements Serializable {
     @NotNull(message = "The onSale cannot be null.")
     private boolean onSale;
 
-    public GoodStateResponse(long timestamp, String requestID, String operation, String from, String to,
-                             String signature, String ownerID, boolean onSale) {
+    private String goodID;
+    private String writerID;
+    private long wts;
+    private int rid;
+    private String writeOperationSignature;
+
+    public GoodStateResponse(long timestamp,
+                             String requestID,
+                             String operation,
+                             String from,
+                             String to,
+                             String signature,
+                             String ownerID,
+                             boolean onSale,  // value associated with write
+                             String goodID,   // goodId
+                             String writerID, // entity that wrote the value goodId, onSale on the database
+                             long wts,        // write time stamp
+                             int rid,         // request identifier which has logical value only on the client side
+                             String writeOperationSignature) // signature over onSale, goodId, writerId, wts)
+    {
         super(timestamp, requestID, operation, from, to, signature);
         this.ownerID = ownerID;
         this.onSale = onSale;
@@ -41,12 +59,57 @@ public class GoodStateResponse extends BasicMessage implements Serializable {
         this.onSale = onSale;
     }
 
+    public String getGoodID() {
+        return goodID;
+    }
+
+    public void setGoodID(String goodID) {
+        this.goodID = goodID;
+    }
+
+    public String getWriterID() {
+        return writerID;
+    }
+
+    public void setWriterID(String writerID) {
+        this.writerID = writerID;
+    }
+
+    public long getWts() {
+        return wts;
+    }
+
+    public void setWts(long wts) {
+        this.wts = wts;
+    }
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public String getWriteOperationSignature() {
+        return writeOperationSignature;
+    }
+
+    public void setWriteOperationSignature(String writeOperationSignature) {
+        this.writeOperationSignature = writeOperationSignature;
+    }
+
     @Override
     public String toString() {
         return "GoodStateResponse{" +
                 "ownerID='" + ownerID + '\'' +
                 ", onSale=" + onSale +
-                ", requestID=" + requestID +
+                ", goodID='" + goodID + '\'' +
+                ", writerID='" + writerID + '\'' +
+                ", wts=" + wts +
+                ", rid=" + rid +
+                ", writeOperationSignature='" + writeOperationSignature + '\'' +
+                ", requestID='" + requestID + '\'' +
                 ", operation='" + operation + '\'' +
                 ", from='" + from + '\'' +
                 ", to='" + to + '\'' +
