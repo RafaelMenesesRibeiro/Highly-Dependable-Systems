372ef19beafa201b3c92922394303af363bd7968
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-11 22:40:58 +0100

    Javadoc

--- hds/server/src/main/java/hds/server/exception/NoPermissionException.java
+++ hds/server/src/main/java/hds/server/exception/NoPermissionException.java
@@ -1,5 +1,10 @@
 package hds.server.exception;
 
+/**
+ * Exception to client has no permission for the transaction it is trying.
+ *
+ * @author 		Rafael Ribeiro
+ */
 public class NoPermissionException extends RuntimeException {
 	public NoPermissionException(String msg) {
 		super(msg);
--- hds/server/src/main/java/hds/server/exception/OldMessageException.java
+++ hds/server/src/main/java/hds/server/exception/OldMessageException.java
@@ -1,5 +1,10 @@
 package hds.server.exception;
 
+/**
+ * Some of the request's timestamps are too old to be relevant.
+ *
+ * @author 		Rafael Ribeiro
+ */
 public class OldMessageException extends RuntimeException{
 	public OldMessageException(String msg) {
 		super(msg);
