d3ce476578325a81d7e52a19a2da2d327d2048a4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-11 22:16:27 +0100

    Update SecurityManager.java

--- hds/security/src/main/java/hds/security/SecurityManager.java
+++ hds/security/src/main/java/hds/security/SecurityManager.java
@@ -53,7 +53,6 @@ public class SecurityManager {
 
     /** Given a basic message it verifies if it's fresh, if it's been seen before and if the signature is valid */
     public static String isValidMessage(BasicMessage message) {
-        // TODO Has this been processed before? Using a UUID - Not a priority
 
         if (!isFreshTimestamp(message.getTimestamp())) {
             return "message is more than five minutes old";
