c7d1d78df69a32276fb21fc62f3d1e7d7f41995f
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-11 15:10:40 +0100

    Overloaded queryDB with returnColumn List. Refactor later

--- hds/server/src/main/java/hds/server/helpers/DatabaseInterface.java
+++ hds/server/src/main/java/hds/server/helpers/DatabaseInterface.java
@@ -3,6 +3,8 @@ package hds.server.helpers;
 import hds.server.exception.DBClosedConnectionException;
 import hds.server.exception.DBConnectionRefusedException;
 import hds.server.exception.DBNoResultsException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.List;
  *
  * @author 		Rafael Ribeiro
  */
+@SuppressWarnings("Duplicates")
 class DatabaseInterface {
 
 	private DatabaseInterface() {
@@ -77,6 +80,64 @@ class DatabaseInterface {
 					throw new DBConnectionRefusedException(sqlex.getMessage());
 				case "02000": // no_data
 					if (!returnColumn.equals("")) {
+						throw new DBNoResultsException(sqlex.getMessage());
+					}
+					break;
+				default:
+					throw sqlex;
+			}
+		}
+		return results;
+	}
+
+	// TODO - Add Javadoc. //
+	static List<JSONObject> queryDB(Connection conn, String query, List<String> returnColumns, List<String> args)
+			throws DBClosedConnectionException, DBConnectionRefusedException, DBNoResultsException, SQLException, JSONException {
+
+		List<JSONObject> results = new ArrayList<>();
+		try (
+				PreparedStatement stmt = conn.prepareStatement(query)) {
+
+			int i = 1;
+			for (String s: args) {
+				if (s.equals("false")) {
+					stmt.setBoolean(i, false);
+				}
+				else if (s.equals("true")) {
+					stmt.setBoolean(i, true);
+				}
+				else {
+					stmt.setString(i, s);
+				}
+				i += 1;
+			}
+
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				JSONObject json = new JSONObject();
+				for (String key: returnColumns) {
+					json.put(key, rs.getString(key));
+				}
+				results.add(json);
+			}
+			rs.close();
+		}
+		catch (SQLTimeoutException sqltex) {
+			throw new DBNoResultsException(sqltex.getMessage());
+		}
+		catch (SQLException sqlex) {
+			switch (sqlex.getSQLState()) {
+				case "08000": // connection_exception
+				case "08003": // connection_does_not_exist
+				case "08006": // connection_failure
+				case "08001": // qlclient_unable_to_establish_sqlconnection
+					throw new DBClosedConnectionException(sqlex.getMessage());
+				case "08004": // sqlserver_rejected_establishment_of_sqlconnection
+				case "08007": // transaction_resolution_unknown
+				case "08P01": // protocol_violation
+					throw new DBConnectionRefusedException(sqlex.getMessage());
+				case "02000": // no_data
+					if (!returnColumns.isEmpty()) {
 						throw new DBNoResultsException(sqlex.getMessage());
 					}
 					break;
