d8cb67b7de6475dc4f4756295a4ddfeefe62ff99
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-12 20:50:55 +0100

    Update CallableManager.java

--- hds/client/src/main/java/hds/client/domain/CallableManager.java
+++ hds/client/src/main/java/hds/client/domain/CallableManager.java
@@ -4,12 +4,21 @@ import hds.security.msgtypes.BasicMessage;
 
 import java.util.concurrent.*;
 
-
+/**
+ * CallableManager is Callable implementation that empowers ExecutorCompletionServices with the ability to run their
+ * callable jobs with timeouts defined for each. This would otherwise be impossible as ExecutionCompletionService only
+ * provides away of defining a global for all tasks to complete or by using the .poll() method, which would give each
+ * job a defined timeout from the moment .poll was made, instead of when the task was launched.
+ * @author Diogo Vilela
+ * @author Francisco Barros
+ * @author Rafael Ribeiro
+ */
 public class CallableManager implements Callable<BasicMessage> {
     protected Callable<BasicMessage> callable;
     protected long timeout;
     protected TimeUnit timeUnit;
 
+    
     public CallableManager(Callable<BasicMessage> callable, long timeout, TimeUnit timeUnit) {
         this.timeout = timeout;
         this.timeUnit = timeUnit;
