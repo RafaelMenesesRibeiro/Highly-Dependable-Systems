6877fd33289235a5847574255f9aee4662a2b163
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-12 21:14:35 +0100

    Javadoc RequestChallengeMessage

--- hds/client/src/main/java/hds/client/domain/RequestChallengeCallable.java
+++ hds/client/src/main/java/hds/client/domain/RequestChallengeCallable.java
@@ -10,17 +10,35 @@ import static hds.client.helpers.ClientProperties.getMyPrivateKey;
 import static hds.client.helpers.ConnectionManager.*;
 import static hds.security.SecurityManager.setMessageSignature;
 
+/**
+ * The type request challenge callable performs a GET request to the end point /requestChallenge of a notary replica
+ * @author Diogo Vilela
+ * @author Francisco Barros
+ * @author Rafael Ribeiro
+ */
 public class RequestChallengeCallable implements Callable<BasicMessage> {
 	private static final String OPERATION = "requestChallenge";
 	private static final String REQUEST_ENDPOINT = "http://localhost:%s/%s";
 	private final BasicMessage message;
 	private final String replicaID;
 
+	/**
+	 * Instantiates a new Request challenge callable.
+	 *
+	 * @param timestamp the timestamp
+	 * @param replicaId the replica id
+	 * @param requestId the request id
+	 */
 	public RequestChallengeCallable(long timestamp, String replicaId, String requestId) {
 		this.replicaID = replicaId;
 		this.message = newRequestChallengeMessage(timestamp, replicaId, requestId);
 	}
 
+	/**
+	 * Executes this callable
+	 * @return BasicMessage
+	 * @throws Exception which can be of type SignatureException, IOException, JsonProcessingException and JSONException
+	 */
 	@Override
 	public BasicMessage call() throws Exception {
 		setMessageSignature(getMyPrivateKey(), message);
@@ -29,6 +47,13 @@ public class RequestChallengeCallable implements Callable<BasicMessage> {
 		return (BasicMessage) getResponseMessage(connection, Expect.CHALLENGE_REQUEST_RESPONSE);
 	}
 
+	/**
+	 * Helper method that instantiates a new BasicMessage
+	 * @param timestamp    long timestamp representing an java epoch from seconds, {@link hds.security.DateUtils#generateTimestamp()}
+	 * @param replicaId    String identifier representing the port where the replica is listening on
+	 * @param requestId    String
+	 * @return {@link hds.security.msgtypes.BasicMessage}
+	 */
 	private BasicMessage newRequestChallengeMessage(long timestamp, String replicaId, String requestId) {
 		return new BasicMessage(timestamp, requestId, OPERATION, getMyClientPort(), replicaId, "");
 	}
