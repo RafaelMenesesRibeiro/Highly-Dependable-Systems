fe7f4f75a111e06ad8535d7ad6c91e4f785a3277
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-12 21:13:06 +0100

    Javadoc TransferGoodCallable

--- hds/client/src/main/java/hds/client/domain/TransferGoodCallable.java
+++ hds/client/src/main/java/hds/client/domain/TransferGoodCallable.java
@@ -12,17 +12,37 @@ import static hds.client.helpers.ClientProperties.getMyPrivateKey;
 import static hds.client.helpers.ConnectionManager.*;
 import static hds.security.SecurityManager.setMessageWrappingSignature;
 
+/**
+ * The type transfer good callable performs a PUT request to the end point /transferGood of a notary replica
+ * @author Diogo Vilela
+ * @author Francisco Barros
+ * @author Rafael Ribeiro
+ */
 public class TransferGoodCallable implements Callable<BasicMessage> {
     private static final String OPERATION = "transferGood";
     private static final String REQUEST_ENDPOINT = "http://localhost:%s/%s";
     private final ApproveSaleRequestMessage message;
     private final String replicaId;
 
+
+    /**
+     * Instantiates a new Transfer good callable.
+     *
+     * @param timestamp         the timestamp
+     * @param replicaId         the replica id
+     * @param requestMessage    the request message
+     * @param challengeResponse the challenge response
+     */
     public TransferGoodCallable(long timestamp, String replicaId, SaleRequestMessage requestMessage, String challengeResponse) {
         this.replicaId = replicaId;
         this.message = newApproveSaleRequestMessage(timestamp, replicaId, requestMessage, challengeResponse);
     }
 
+    /**
+     * Executes this callable
+     * @return BasicMessage
+     * @throws Exception which can be of type SignatureException, IOException, JsonProcessingException and JSONException
+     */
     @Override
     public BasicMessage call() throws Exception {
         setMessageWrappingSignature(getMyPrivateKey(), message);
@@ -31,6 +51,14 @@ public class TransferGoodCallable implements Callable<BasicMessage> {
         return (BasicMessage) getResponseMessage(connection, Expect.SALE_CERT_RESPONSE);
     }
 
+    /**
+     * Helper method that instantiates a new ApproveSaleRequestMessage
+     * @param timestamp         long timestamp representing an java epoch from seconds, {@link hds.security.DateUtils#generateTimestamp()}
+     * @param replicaId         String identifier representing the port where the replica is listening on
+     * @param requestMessage    {@link hds.security.msgtypes.SaleRequestMessage}
+     * @param challengeResponse String with what is believed to be the solution for the computational challenge provided by the server
+     * @return {@link hds.security.msgtypes.ApproveSaleRequestMessage}
+     */
     private ApproveSaleRequestMessage newApproveSaleRequestMessage(long timestamp,
                                                                    String replicaId,
                                                                    SaleRequestMessage requestMessage,
