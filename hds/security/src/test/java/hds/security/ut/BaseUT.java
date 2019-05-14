da5d5990e24487f6d14de73ebc22f3831120847f
DiogoFSVilela <diogofsvilela@gmail.com>
2019-05-14 14:19:02 +0100

    Add basic SignNVerify unit tests

---
+++ hds/security/src/test/java/hds/security/ut/BaseUT.java
@@ -0,0 +1,29 @@
+package hds.security.ut;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import static hds.security.ResourceManager.getPrivateKeyFromResource;
+import static hds.security.ResourceManager.getPublicKeyFromResource;
+import static org.junit.Assert.fail;
+
+public class BaseUT {
+    private static final String C_1_PORT = "8001";
+    private static final String C_2_PORT = "8002";
+
+    static PrivateKey c1PrivateKey;
+    static PublicKey c1PublicKey;
+    static PrivateKey c2PrivateKey;
+    static PublicKey c2PublicKey;
+
+    static {
+        try {
+            c1PrivateKey = getPrivateKeyFromResource(C_1_PORT);
+            c1PublicKey = getPublicKeyFromResource(C_1_PORT);
+            c2PrivateKey = getPrivateKeyFromResource(C_2_PORT);
+            c2PublicKey = getPublicKeyFromResource(C_2_PORT);
+        } catch (Exception e) {
+            fail("Could not load public/private key pairs from resource...");
+        }
+    }
+}
