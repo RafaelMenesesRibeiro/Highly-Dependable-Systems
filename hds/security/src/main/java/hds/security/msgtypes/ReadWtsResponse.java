1d26eea8d4461cb6113cb2f8dfae6cb1d65fbf03
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-14 01:50:18 +0100

    Update ReadWtsResponse.java

--- hds/security/src/main/java/hds/security/msgtypes/ReadWtsResponse.java
+++ hds/security/src/main/java/hds/security/msgtypes/ReadWtsResponse.java
@@ -7,13 +7,11 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public class ReadWtsResponse extends BasicMessage implements Serializable {
-    @NotNull(message = "The clientID cannot be null.")
-    @NotEmpty(message = "The clientID cannot be empty.")
-    @ValidClientID
-    private String clientID;
-
     @NotNull(message = "The readID cannot be null.")
     private int rid;
+
+    @NotNull(message = "The readID cannot be null.")
+    private long wts;
 
     public ReadWtsResponse(long timestamp,
                            String requestID,
@@ -21,23 +19,15 @@ public class ReadWtsResponse extends BasicMessage implements Serializable {
                            String from,
                            String to,
                            String signature,
-                           String clientID,
-                           int rid)
+                           int rid,
+                           long wts)
     {
         super(timestamp, requestID, operation, from, to, signature);
-        this.clientID = clientID;
         this.rid = rid;
+        this.wts = wts;
     }
 
     public ReadWtsResponse() {
-    }
-
-    public String getClientID() {
-        return clientID;
-    }
-
-    public void setClientID(String clientID) {
-        this.clientID = clientID;
     }
 
     public int getRid() {
@@ -48,11 +38,19 @@ public class ReadWtsResponse extends BasicMessage implements Serializable {
         this.rid = rid;
     }
 
+    public long getWts() {
+        return wts;
+    }
+
+    public void setWts(long wts) {
+        this.wts = wts;
+    }
+
     @Override
     public String toString() {
         return "ReadWtsResponse{" +
-                "clientID='" + clientID + '\'' +
-                ", rid=" + rid +
+                "rid=" + rid +
+                ", wts=" + wts +
                 ", requestID='" + requestID + '\'' +
                 ", operation='" + operation + '\'' +
                 ", from='" + from + '\'' +
