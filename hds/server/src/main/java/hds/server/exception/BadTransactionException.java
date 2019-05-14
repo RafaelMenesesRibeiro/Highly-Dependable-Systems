a7269483cd75e19e7419adc63120dd90180a4a5d
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-14 12:41:20 +0100

    Javadoc

--- hds/server/src/main/java/hds/server/exception/BadTransactionException.java
+++ hds/server/src/main/java/hds/server/exception/BadTransactionException.java
@@ -1,5 +1,10 @@
 package hds.server.exception;
 
+/**
+ * Exception to represent a bad transaction.
+ *
+ * @author 		Rafael Ribeiro
+ */
 public class BadTransactionException extends RuntimeException {
 	public BadTransactionException(String msg) {
 		super(msg);
--- hds/server/src/main/java/hds/server/exception/FailedWriteBackException.java
+++ hds/server/src/main/java/hds/server/exception/FailedWriteBackException.java
@@ -1,5 +1,11 @@
 package hds.server.exception;
 
+/**
+ * Exception to represent the write back operation is not valid / failed.
+ * Usually because the given write timestamp is inferior to the replica's write timestamp.
+ *
+ * @author 		Rafael Ribeiro
+ */
 public class FailedWriteBackException extends RuntimeException {
 	public FailedWriteBackException(String msg) {
 		super(msg);
