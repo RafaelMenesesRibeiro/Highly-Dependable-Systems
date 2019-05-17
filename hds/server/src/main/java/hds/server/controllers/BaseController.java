4e7697b9393133d6ef15781197e4d84e99b57e3f
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-17 05:53:42 +0100

    Javadoc

--- hds/server/src/main/java/hds/server/controllers/BaseController.java
+++ hds/server/src/main/java/hds/server/controllers/BaseController.java
@@ -12,6 +12,17 @@ public abstract class BaseController {
 	protected static final String FROM_SERVER = ServerApplication.getPort();
 	public String OPERATION = "BASE_CONTROLLER";
 
+	/**
+	 * Responsible for the core of the controller. Overridden in every child class.
+	 *
+	 * @param 	requestData			ApproveSaleRequestMessage
+	 * @return 	MetaResponse 		Contains an HttpStatus code and a BasicMessage
+	 * @throws 	JSONException					Can't create / parse JSONObject
+	 * @throws 	SQLException					The DB threw an SQLException
+	 * @throws 	DBClosedConnectionException		Can't access the DB
+	 * @throws 	DBConnectionRefusedException	Can't access the DB
+	 * @throws 	DBNoResultsException			The DB did not return any results
+	 */
 	public abstract MetaResponse execute(BasicMessage requestData)
 			throws JSONException, SQLException, DBClosedConnectionException, DBConnectionRefusedException,
 			DBNoResultsException, OldMessageException, NoPermissionException;
